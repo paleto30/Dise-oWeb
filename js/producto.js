4185fdbce0a5f9bee70e0e8a42651325ed6c04c9
Paleto <paleto0817@gmail.com>
2022-09-12 02:15:45 -0500

    imagenes

--- js/producto.js
+++ js/producto.js
@@ -43,26 +43,26 @@ const getDrinks = async () =>{
 
     try {
 
-        const response = await fetch(url);
+        const response = await fetch(url1);
         const result = await response.json();
         console.log(result.drink);
-        const contenService = document.querySelector("#drink");
+        const contenDrink = document.querySelector("#drink");
         let html = "";
 
         result.drink.forEach(element => {
-            const {nom, img, ingred} = element;
+            const {nomb, img} = element;
             html +=`
             <div class="col-12 col-md-4 text-center cards">
                 <div class="card cd-pro" style="width:500px">
                     <div class="card-body">
-                        <h2 class="card-title">${nom}</h2>
+                        <h2 class="card-title">${nomb}</h2>
                         <img class="card-img-bottom" src="${img}" alt="Card image" style="width:100%; border-radius: 0.3em;">      
-                        <p class="card-text mt-2">${ingred}</p>      
+                        <p class="card-text mt-2"></p>      
                     </div>     
                 </div>
             </div>
             `;
-            contenService.innerHTML = html;
+            contenDrink.innerHTML = html;
         });
     
     } catch (error) {  
