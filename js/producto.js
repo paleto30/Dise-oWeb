1577089939def4996659c76c92eaaecdd5e5513e
Paleto <paleto0817@gmail.com>
2022-09-12 09:53:44 -0500

    prodcutos terminada

--- js/producto.js
+++ js/producto.js
@@ -19,15 +19,15 @@ const getProduct = async () =>{
         result.food.forEach(element => {
             const {nom, img, ingred} = element;
             html +=`
-            <div class="col-12 col-md-4 text-center cards">
-                <div class="card cd-pro" style="width:500px">
-                    <div class="card-body">
-                        <h2 class="card-title">${nom}</h2>
-                        <img class="card-img-bottom" src="${img}" alt="Card image" style="width:100%">      
-                        <p class="card-text mt-2">${ingred}</p>      
-                    </div>     
+                <div class="col-12 col-md-4 text-center cards">
+                    <div class="card cd-pro" style="width:500px">
+                        <div class="card-body">
+                            <h2 class="card-title">${nom}</h2>
+                            <img class="card-img-bottom" src="${img}" alt="Card image" style="width:100%">      
+                            <p class="card-text mt-2">${ingred}</p>      
+                        </div>     
+                    </div>
                 </div>
-            </div>
             `;
             contenService.innerHTML = html;
         });
@@ -35,39 +35,39 @@ const getProduct = async () =>{
     } catch (error) {  
         console.log(error);
     }
+};
 
-}
 
-const url1 = "data/product.json";
-const getDrinks = async () =>{
+
+/// funcion de las bebidas
+
+const getDrinks =  async () =>{
 
     try {
-
-        const response = await fetch(url1);
+        const response = await fetch(url);
         const result = await response.json();
-        console.log(result.drink);
         const contenDrink = document.querySelector("#drink");
         let html = "";
 
         result.drink.forEach(element => {
-            const {nomb, img} = element;
+            const {title,img} = element;
             html +=`
             <div class="col-12 col-md-4 text-center cards">
-                <div class="card cd-pro" style="width:500px">
-                    <div class="card-body">
-                        <h2 class="card-title">${nomb}</h2>
-                        <img class="card-img-bottom" src="${img}" alt="Card image" style="width:100%; border-radius: 0.3em;">      
-                        <p class="card-text mt-2"></p>      
-                    </div>     
-                </div>
+            <div class="card cd-pro" style="width:500px">
+                <div class="card-body">
+                    <h2 class="card-title">${title}</h2>
+                    <img class="card-img-bottom" src="${img}" alt="Card image" style="width:100%">           
+                </div>     
             </div>
+        </div>
             `;
+
             contenDrink.innerHTML = html;
         });
-    
-    } catch (error) {  
+    } catch (error) {
         console.log(error);
     }
 
 }
+
 
